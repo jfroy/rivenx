ca0ba0eab7bc7531909377c1d7015d5197e150eb
Jean-Francois Roy <jeanfrancois.roy@me.com>
2010-01-30 16:45:25 -0800

    New NSWindow subclass to better handle fullscreen.

---
+++ Rendering/Graphics/RXWindow.h
@@ -0,0 +1,16 @@
+//
+//  RXWindow.h
+//  rivenx
+//
+//  Created by Jean-Francois Roy on 30/01/2010.
+//  Copyright 2010 MacStorm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface RXWindow : NSWindow {
+
+}
+
+@end
---
+++ Rendering/Graphics/RXWindow.m
@@ -0,0 +1,22 @@
+//
+//  RXWindow.m
+//  rivenx
+//
+//  Created by Jean-Francois Roy on 30/01/2010.
+//  Copyright 2010 MacStorm. All rights reserved.
+//
+
+#import "RXWindow.h"
+
+
+@implementation RXWindow
+
+- (BOOL)canBecomeKeyWindow {
+    return YES;
+}
+
+- (BOOL)canBecomeMainWindow {
+    return YES;
+}
+
+@end
