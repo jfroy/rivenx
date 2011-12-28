44705c615110404906383aca200c7ec4894d3b59
Jean-Francois Roy <jeanfrancois.roy@me.com>
2011-12-28 00:18:32 -0800

    Add array additions file.
    Currently adds one method to fetch an object at index or nil if the index is out of bounds.

---
+++ Utilities/NSArray+RXArrayAdditions.h
@@ -0,0 +1,16 @@
+//
+//  NSArray+RXArrayAdditions.h
+//  rivenx
+//
+//  Created by Jean-François Roy on 26/12/2011.
+//  Copyright (c) 2011 MacStorm. All rights reserved.
+//
+
+#import <Foundation/NSArray.h>
+
+
+@interface NSArray (RXArrayAdditions)
+
+- (id)objectAtIndexIfAny:(NSUInteger)index;
+
+@end
---
+++ Utilities/NSArray+RXArrayAdditions.m
@@ -0,0 +1,21 @@
+//
+//  NSArray+RXArrayAdditions.m
+//  rivenx
+//
+//  Created by Jean-François Roy on 26/12/2011.
+//  Copyright (c) 2011 MacStorm. All rights reserved.
+//
+
+#import "NSArray+RXArrayAdditions.h"
+
+
+@implementation NSArray (RXArrayAdditions)
+
+- (id)objectAtIndexIfAny:(NSUInteger)index
+{
+    if ([self count] <= index)
+        return nil;
+    return [self objectAtIndex:index];
+}
+
+@end
