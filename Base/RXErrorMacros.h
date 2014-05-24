4c52f951c11e48ef1e822428f818e6374624cfa7
Jean-Francois Roy <jeanfrancois.roy@me.com>
2014-05-24 12:33:52 -0700

    Fix crazy formatting in RXErrorMacros.

--- Base/RXErrorMacros.h
+++ Base/RXErrorMacros.h
@@ -13,43 +13,43 @@
 #define RX_ERROR_CLASS NSError
 #endif
 
-#define ReturnWithError(errorDomain, errorCode, errorInfo, errorPtr)                                                                                           \
-  do {                                                                                                                                                         \
-    if ((errorPtr))                                                                                                                                            \
-      *(errorPtr) = [RX_ERROR_CLASS errorWithDomain:(errorDomain)code:(errorCode)userInfo:(errorInfo)];                                                        \
-    return;                                                                                                                                                    \
+#define ReturnWithError(errorDomain, errorCode, errorInfo, errorPtr)                                     \
+  do {                                                                                                   \
+    if ((errorPtr))                                                                                      \
+      *(errorPtr) = [RX_ERROR_CLASS errorWithDomain:(errorDomain)code:(errorCode)userInfo:(errorInfo)];  \
+    return;                                                                                              \
   } while (0)
 
-#define ReturnValueWithError(value, errorDomain, errorCode, errorInfo, errorPtr)                                                                               \
-  do {                                                                                                                                                         \
-    if ((errorPtr))                                                                                                                                            \
-      *(errorPtr) = [RX_ERROR_CLASS errorWithDomain:(errorDomain)code:(errorCode)userInfo:(errorInfo)];                                                        \
-    return (value);                                                                                                                                            \
+#define ReturnValueWithError(value, errorDomain, errorCode, errorInfo, errorPtr)                         \
+  do {                                                                                                   \
+    if ((errorPtr))                                                                                      \
+      *(errorPtr) = [RX_ERROR_CLASS errorWithDomain:(errorDomain)code:(errorCode)userInfo:(errorInfo)];  \
+    return (value);                                                                                      \
   } while (0)
 
-#define ReturnValueWithError2(VALUE, CLASS, DOMAIN, CODE, INFO, PTR)                                                                                           \
-  do {                                                                                                                                                         \
-    if ((PTR))                                                                                                                                                 \
-      *(PTR) = [CLASS errorWithDomain:(DOMAIN)code:(CODE)userInfo:(INFO)];                                                                                     \
-    return (VALUE);                                                                                                                                            \
+#define ReturnValueWithError2(VALUE, CLASS, DOMAIN, CODE, INFO, PTR)                                     \
+  do {                                                                                                   \
+    if ((PTR))                                                                                           \
+      *(PTR) = [CLASS errorWithDomain:(DOMAIN)code:(CODE)userInfo:(INFO)];                               \
+    return (VALUE);                                                                                      \
   } while (0)
 
-#define SetErrorToPOSIXError(errorInfo, errorPtr)                                                                                                              \
-  do {                                                                                                                                                         \
-    if ((errorPtr))                                                                                                                                            \
-      *(errorPtr) = [RX_ERROR_CLASS errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:(errorInfo)];                                                       \
+#define SetErrorToPOSIXError(errorInfo, errorPtr)                                                        \
+  do {                                                                                                   \
+    if ((errorPtr))                                                                                      \
+      *(errorPtr) = [RX_ERROR_CLASS errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:(errorInfo)]; \
   } while (0)
 
-#define ReturnWithPOSIXError(errorInfo, errorPtr)                                                                                                              \
-  do {                                                                                                                                                         \
-        SetErrorToPOSIXError((errorInfo), (errorPtr());                                                                     \
-        return;                                                                                                                                                \
+#define ReturnWithPOSIXError(errorInfo, errorPtr)                                                        \
+  do {                                                                                                   \
+    SetErrorToPOSIXError((errorInfo), (errorPtr());                                                      \
+    return;                                                                                              \
   } while (0)
 
-#define ReturnValueWithPOSIXError(value, errorInfo, errorPtr)                                                                                                  \
-  do {                                                                                                                                                         \
-    SetErrorToPOSIXError((errorInfo), (errorPtr));                                                                                                             \
-    return (value);                                                                                                                                            \
+#define ReturnValueWithPOSIXError(value, errorInfo, errorPtr)                                            \
+  do {                                                                                                   \
+    SetErrorToPOSIXError((errorInfo), (errorPtr));                                                       \
+    return (value);                                                                                      \
   } while (0)
 
 #endif // RX_ERROR_MACROS_H
