754fe7792321614d05e635714a044a8bd09cb2b5
Jean-Francois Roy <jeanfrancois.roy@me.com>
2014-08-06 19:35:33 -0700

    Safer buffer macros for C++

--- Base/RXBufferMacros.h
+++ Base/RXBufferMacros.h
@@ -1,23 +1,75 @@
-//
-//  RXBufferMacros.h
-//  rivenx
-//
+// Copyright 2014 Jean-Francois Roy. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-#if !defined(RX_BUFFER_MACROS_H)
-#define RX_BUFFER_MACROS_H
+#pragma once
+
+#if defined(__cplusplus)
+
+#include <type_traits>
+
+namespace rx {
+
+template <typename T, typename U>
+inline
+typename std::enable_if<std::is_integral<T>::value, T>::type
+BUFFER_OFFSET(T buffer, U bytes) {
+	return static_cast<T>(buffer + bytes);
+}
+
+template <typename T, typename U>
+inline
+typename std::enable_if<std::is_pointer<T>::value, T>::type
+BUFFER_OFFSET(T buffer, U bytes) {
+	return reinterpret_cast<T>(BUFFER_OFFSET(reinterpret_cast<uintptr_t>(buffer), bytes));
+}
+
+template <typename T>
+inline size_t
+BUFFER_DELTA(typename std::enable_if<std::is_integral<T>::value, T>::type head_ptr, T read_ptr) {
+	return read_ptr - head_ptr;
+}
+
+template <typename T>
+inline size_t
+BUFFER_DELTA(typename std::enable_if<std::is_pointer<T>::value, T>::type head_ptr, T read_ptr) {
+	return BUFFER_DELTA(reinterpret_cast<uintptr_t>(head_ptr), reinterpret_cast<uintptr_t>(read_ptr));
+}
+
+template <typename T>
+inline
+typename std::enable_if<std::is_unsigned<T>::value, T>::type
+BUFFER_ALIGN(T buffer, size_t a) {
+	T mask = static_cast<T>(a - 1);
+	return (buffer + mask) & (~mask);
+}
+
+template <typename T>
+inline
+typename std::enable_if<std::is_pointer<T>::value, T>::type
+BUFFER_ALIGN(T buffer, size_t a) {
+	return reinterpret_cast<T>(BUFFER_ALIGN(reinterpret_cast<uintptr_t>(buffer), a));
+}
+
+template <typename T>
+inline void BUFFER_ADD_BYTES(T& buffer, size_t bytes) {
+	buffer = BUFFER_OFFSET(buffer, bytes);
+}
+
+template <typename T>
+inline size_t BUFFER_ALIGN_SIZE(T buffer, size_t a) {
+	return BUFFER_DELTA(buffer, BUFFER_ALIGN(buffer, a));
+}
+
+} // namespace rx {
+
+#else // defined(__cplusplus)
 
 #define BUFFER_OFFSET(buffer, bytes) (__typeof__(buffer))((uintptr_t)(buffer) + (bytes))
-#define BUFFER_NOFFSET(buffer, bytes) (__typeof__(buffer))((uintptr_t)(buffer) - (bytes))
-#define BUFFER_ADD_OFFSET(buffer, bytes) (buffer) = BUFFER_OFFSET((buffer), (bytes))
+#define BUFFER_ADD_BYTES(buffer, bytes) (buffer) = (__typeof__(buffer))(((uintptr_t)(buffer)) + (bytes))
 
 #define BUFFER_DELTA(head_ptr, read_ptr) ((size_t)(((uintptr_t)(read_ptr)) - ((uintptr_t)(head_ptr))))
 
-#define BUFFER_ALIGN(buffer, a) (__typeof__(buffer))(((uintptr_t)(buffer) + (a - 1ul)) & ~(a - 1ul))
+#define BUFFER_ALIGN(buffer, a) (__typeof__(buffer))(((uintptr_t)(buffer) + ((a)-1ul)) & ~((a)-1ul))
 #define BUFFER_ALIGN_SIZE(buffer, a) (size_t)((uintptr_t)BUFFER_ALIGN((buffer), (a)) - (uintptr_t)(buffer))
 
-#define BUFFER_ALIGN2(buffer) BUFFER_ALIGN(buffer, 2ul)
-#define BUFFER_ALIGN4(buffer) BUFFER_ALIGN(buffer, 4ul)
-#define BUFFER_ALIGN8(buffer) BUFFER_ALIGN(buffer, 8ul)
-#define BUFFER_ALIGN16(buffer) BUFFER_ALIGN(buffer, 16ul)
-
-#endif // RX_BUFFER_MACROS_H
+#endif
