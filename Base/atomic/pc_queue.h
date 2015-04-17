1b9bb50d23563eb225c5f736ac56f5f4298ea39c
Jean-Francois Roy <jf@devklog.net>
2015-04-16 23:03:10 -0700

    WIP: Base atomic data structures.
    This commit includes a more complete version of FixedSinglePCQueue that has working core enqueue and dequeue operations. There are some more tests for it as well.

--- Base/atomic/pc_queue.h
+++ Base/atomic/pc_queue.h
@@ -21,7 +21,9 @@ namespace atomic {
 // (for example if there is not enough memory), there will be no observable change to the queue.
 //
 // The queue never allocates memory in enqueue or dequeue operations. It initially has no capacity
-// and must be resized before use. Resizing to 0 will deallocate all heap memory.
+// and must be resized before use. Resizing to 0 will deallocate all heap memory. An extra element
+// is allocated to avoid the "empty or full" state overlap (equal enqueue and dequeue pointers) by
+// always keeping one slot empty.
 //
 // The resize operation is not thread safe. The initial owner of the queue must resize it before
 // publishing it to other threads. Similarly, destruction of the queue is not thread safe and must
@@ -31,9 +33,6 @@ namespace atomic {
 // By design, there can be only one consumer and one producer thread without locking. Any other
 // configuration requires use of external serialization (with the producer and consumer sides being
 // independent of each other).
-//
-// The queue always keeps one slot empty to avoid the "empty or full" confusion when the enqueue and
-// dequeue pointers are equal. The effective capacity is therefore one less than requested.
 template <typename T>
 class FixedSinglePCQueue : public noncopyable {
  public:
@@ -41,27 +40,49 @@ class FixedSinglePCQueue : public noncopyable {
   using reference = value_type&;
   using pointer = value_type*;
   using range = std::pair<pointer, pointer>;
-  using span = std::pair<range, range>;
+  using range_pair = std::pair<range, range>;
+  using size_pair = std::pair<ssize_t, ssize_t>;
 
   // Estimated cache line size. If this is too large, no problem. But if too big, it will lead to
   // false sharing and decreased performance.
-  static constexpr size_t cache_line_size() { return 64; }
+  static const int CACHE_LINE_SIZE = 64;
 
   FixedSinglePCQueue() = default;
 
   // Resize the queue to the specified capacity. No memory allocation will be performed beyond this
   // operation. Passing 0 will deallocate all memory. Not thread safe at all.
   void resize(size_t capacity) {
+    pointer begin, end;
+
     buffer_.clear();
-    buffer_.resize(capacity);
+    if (capacity > 0) {
+      buffer_.resize(capacity + 1);
+      begin = &buffer_.front();
+      end = &buffer_.back() + 1;
+    } else {
+      buffer_.shrink_to_fit();
+      begin = nullptr;
+      end = nullptr;
+    }
 
-    enqueue_begin_ = std::begin(buffer_);
-    enqueue_end_ = std::end(buffer_);
-    enqueue_ptr_.store(enqueue_begin_, std::memory_order_relaxed);
+    enqueue_.ptr.store(begin, std::memory_order_relaxed);
+    enqueue_.other_ptr_cache = begin;
+    const_cast<pointer&>(enqueue_.begin) = begin;
+    const_cast<pointer&>(enqueue_.end) = end;
 
-    dequeue_begin_ = std::begin(buffer_);
-    dequeue_end_ = std::end(buffer_);
-    dequeue_ptr_.store(dequeue_begin_, std::memory_order_relaxed);
+    dequeue_.ptr.store(begin, std::memory_order_relaxed);
+    dequeue_.other_ptr_cache = begin;
+    const_cast<pointer&>(dequeue_.begin) = begin;
+    const_cast<pointer&>(dequeue_.end) = end;
+
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+  }
+
+  // Try to enqueue the element. Returns false if the operation can't be completed (for example if
+  // there is not enough space).
+  bool try_enqueue(T const& element) {
+    // FIXME: optimize single element enqueue by replacing the range calculation and copy path.
+    return try_enqueue(&element, 1);
   }
 
   // Try to enqueue elements in the range [first, last). Returns false if the operation can't be
@@ -84,71 +105,139 @@ class FixedSinglePCQueue : public noncopyable {
       return true;
     }
 
-    // Calculate the number of slots available on the right and left of the enqueue pointer.
+    // Calculate the number of available elements on the right and left of the enqueue pointer.
     // Generally speaking, the enqueue pointer cannot move forward past the dequeue pointer. Remove
     // one slot from the end of the range to avoid the "empty or full" state overlap.
-    ssize_t right_avail;
-    ssize_t left_avail;
+    pointer enqueue_ptr;
+    auto available = right_left_available_weak(enqueue_, enqueue_ptr);
 
-    // Acquire-load the enqueue pointer and then load the dequeue pointer.
-    auto local_enqueue_ptr = enqueue_ptr_.load(std::memory_order_acquire);
-    auto local_dequeue_ptr = dequeue_ptr_.load(std::memory_order_relaxed);
-
-    // If the enqueue pointer is on the left of the dequeue pointer (i.e. behind), there is space
-    // available only on the right of the enqueue pointer (i.e. forward).
-    if (local_enqueue_ptr < local_dequeue_ptr) {
-      right_avail = local_dequeue_ptr - local_enqueue_ptr;
-      left_avail = 0;
+    // If there is not enough space for all the elements, update the other pointer by loading from
+    // the dequeue cache line and calculate again.
+    if (available.first + available.second < nelements + 1) {
+      enqueue_.other_ptr_cache = dequeue_.ptr.load(std::memory_order_relaxed);
+      available = right_left_available_weak(enqueue_, enqueue_ptr);
     }
 
-    // Otherwise the enqueue pointer is on the right of the dequeue pointer (i.e. ahead) and there
-    // is space both on the right of the enqueue pointer (i.e. forward) and on the left of the it
-    // (i.e. behind).
-    else {
-      right_avail = enqueue_end_ - local_enqueue_ptr;
-      left_avail = local_dequeue_ptr - enqueue_begin_;
-    }
-
-    // Remove one from the end of the range to keep one slot open (see above for explanation).
-    if (right_avail > 0) {
-      --right_avail;
-    } else {
-      --left_avail;
-    }
-
-    // If there is not enough space for all the elements, fail the entire operation.
-    if (right_avail + left_avail < nelements) {
+    // If there is still not enough space, fail the entire transaction.
+    if (available.first + available.second < nelements + 1) {
       return false;
     }
 
+    // Acquire fence. Prevents memory operations from being reordered above it.
+    std::atomic_thread_fence(std::memory_order_acquire);
+
     // Copy elements from the input iterator starting with the right (i.e. forward) range.
-    auto ncopy = std::min(right_avail, nelements);
-    auto new_enqueue_ptr = std::copy_n(first, ncopy, local_enqueue_ptr);
+    auto ncopy = std::min(available.first, nelements);
+    pointer new_enqueue_ptr = std::copy_n(first, ncopy, enqueue_ptr);
 
     // Copy the remainder of the input (if any) in the left (i.e. behind) range.
     if (ncopy < nelements) {
       std::advance(first, ncopy);
       ncopy = nelements - ncopy;
-      new_enqueue_ptr = std::copy_n(first, ncopy, enqueue_begin_);
+      new_enqueue_ptr = std::copy_n(first, ncopy, enqueue_.begin);
     }
 
-    // Publish new elements by Release-storing the new enqueue pointer.
-    enqueue_ptr_.store(new_enqueue_ptr, std::memory_order_release);
+    // Prevent all preceding memory operations from being reordered past subsequent writes.
+    std::atomic_thread_fence(std::memory_order_release);
+
+    // Publish the new enqueue pointer.
+    enqueue_.ptr.store(new_enqueue_ptr, std::memory_order_relaxed);
 
     // All done.
     return true;
   }
 
- private:
-  std::atomic<pointer> enqueue_ptr_{0};  // Owned (modified) by the producer. Read by all.
-  pointer const enqueue_begin_{0};       // Copy of buffer start for producer.
-  pointer const enqueue_end_{0};         // Copy of buffer end for producer.
-  static_assert(sizeof(enqueue_ptr_) == sizeof(pointer), "atomic pointer size != pointer size");
-  int8_t enqueue_cache_line_filler[cache_line_size() - (sizeof(pointer) * 3)];
+  // Return the range of elements that can be dequeued. First only looks at the dequeue cache line.
+  // If the range is empty, the enqueue cache line is read. Does not update the dequeue cache line
+  // (i.e. does not consume the elements).
+  const range_pair& dequeue_peek() {
+    // Calculate the number of available elements on the right and left of the dequeue pointer.
+    // Generally speaking, the dequeue pointer cannot move forward past the enqueue pointer.
+    size_pair available;
 
-  std::atomic<pointer> dequeue_ptr_{0};  // Owned (modified) by the consumer. Read by all.
-  pointer const dequeue_begin_{0};       // Copy of buffer start for consumer.
-  pointer const dequeue_end_{0};         // Copy of buffer end for consumer.
+    // If the pointers are equal, it indicates an empty dequeue range. Try to update the other
+    // pointer by loading from the enqueue cache line and calculate again.
+    pointer dequeue_ptr = dequeue_.ptr.load(std::memory_order_relaxed);
+    if (dequeue_ptr == dequeue_.other_ptr_cache) {
+      dequeue_.other_ptr_cache = enqueue_.ptr.load(std::memory_order_relaxed);
+      if (dequeue_ptr == dequeue_.other_ptr_cache) {
+        available = {0, 0};
+      } else {
+        available = right_left_available_weak(dequeue_, dequeue_ptr);
+      }
+    } else {
+      available = right_left_available_weak(dequeue_, dequeue_ptr);
+    }
+
+    // Update the dequeue range_pair.
+    dequeue_.dequeue_rp.first = {dequeue_ptr, dequeue_ptr + available.first};
+    if (available.second == 0) {
+      dequeue_.dequeue_rp.second = {nullptr, nullptr};
+    } else {
+      dequeue_.dequeue_rp.second = {dequeue_.begin, dequeue_.begin + available.second};
+    }
+
+    // Acquire fence. Prevents memory operations from being reordered above it.
+    std::atomic_thread_fence(std::memory_order_acquire);
+
+    // Return a reference to the range_pair.
+    return dequeue_.dequeue_rp;
+  }
+
+  // Consume the last range_pair returned by dequeue_peek and update the dequeue cache line.
+  void dequeue_consume() {
+    pointer new_dequeue_ptr = (dequeue_.dequeue_rp.second.second)
+                                  ? dequeue_.dequeue_rp.second.second
+                                  : dequeue_.dequeue_rp.first.second;
+
+    // Prevent all preceding memory operations from being reordered past subsequent writes.
+    std::atomic_thread_fence(std::memory_order_release);
+
+    // Publish the new dequeue pointer.
+    dequeue_.ptr.store(new_dequeue_ptr, std::memory_order_relaxed);
+  }
+
+ private:
+  struct alignas(CACHE_LINE_SIZE) CacheLineData {
+    std::atomic<pointer> ptr{0};  // Atomic access pointer.
+    pointer other_ptr_cache{0};   // Local cache of the other access pointer.
+    pointer const begin{0};       // Cache of buffer start.
+    pointer const end{0};         // Cache of buffer end.
+    range_pair dequeue_rp;        // Last dequeue range_pair. Only used by consumer.
+  };
+  static_assert(sizeof(CacheLineData) == CACHE_LINE_SIZE, "CacheLineData size != CACHE_LINE_SIZE");
+
+  // Calculate the space available on the right (pair.first) and left (pair.second) of the specified
+  // cache line's owned pointer (i.e. either the enqueue or dequeue pointer). Also return by
+  // reference the owned pointer (to avoid multiple atomic loads). Can remove one from the end of
+  // the range if requested (used for enqueue to avoid "empty or full" state overlap). The function
+  // uses the cache line's copy of the other cache line's owned pointer for the calculation.
+  size_pair right_left_available_weak(const CacheLineData& cld, pointer& out_ptr) {
+    size_pair available;
+
+    // Load the pointer and cache of the other pointer without ordering.
+    out_ptr = cld.ptr.load(std::memory_order_relaxed);
+    auto other_ptr = cld.other_ptr_cache;
+
+    // If the pointer is on the left of the other pointer (i.e. behind), there is space available
+    // only on the right of the pointer (i.e. forward).
+    if (out_ptr < other_ptr) {
+      available.first = other_ptr - out_ptr;
+      available.second = 0;
+    }
+
+    // Otherwise the pointer is on the right of the other pointer (i.e. ahead) or equal and there
+    // is space both on the right (i.e. forward) and on the left of it (i.e. behind).
+    else {
+      available.first = cld.end - out_ptr;
+      available.second = other_ptr - cld.begin;
+    }
+
+    return available;
+  }
+
+  CacheLineData enqueue_;  // Enqueue (producer) cache line.
+  CacheLineData dequeue_;  // Dequeue (consumer) cache line.
 
   std::vector<T> buffer_;  // Storage for elements.
 };
---
+++ Tests/atomic_test.cc
@@ -0,0 +1,108 @@
+// Copyright 2015 Jean-Francois Roy. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+#include <iostream>
+#include <random>
+
+#include "Base/atomic/pc_queue.h"
+
+using namespace std;
+
+random_device rd;
+default_random_engine re(rd());
+using queue = rx::atomic::FixedSinglePCQueue<decltype(re)::result_type>;
+
+static void assert_empty_rp(const queue::range_pair& rp) {
+  release_assert(rp.first.first == rp.first.second && rp.second.first == rp.second.second);
+}
+
+static void test_enqueue_noresize() {
+  queue q;
+  auto val = re();
+  release_assert(q.try_enqueue(val) == false);
+}
+
+static void test_peek_noresize() {
+  queue q;
+  auto rp = q.dequeue_peek();
+  assert_empty_rp(rp);
+}
+
+static void test_consume_noresize() {
+  queue q;
+  q.dequeue_consume();
+}
+
+static void test_enqueue_peek() {
+  queue q;
+  q.resize(1);
+  auto val = re();
+  release_assert(q.try_enqueue(val));
+  auto rp = q.dequeue_peek();
+  release_assert(*rp.first.first == val);
+}
+
+static void test_enqueue_peek_enqueue() {
+  queue q;
+  q.resize(1);
+  auto val = re();
+  release_assert(q.try_enqueue(val));
+  auto rp = q.dequeue_peek();
+  release_assert(*rp.first.first == val);
+  release_assert(q.try_enqueue(val) == false);
+}
+
+static void test_enqueue_peek_consume_peak() {
+  queue q;
+  q.resize(1);
+  auto val = re();
+  release_assert(q.try_enqueue(val));
+  auto rp = q.dequeue_peek();
+  release_assert(*rp.first.first == val);
+  q.dequeue_consume();
+  rp = q.dequeue_peek();
+  assert_empty_rp(rp);
+}
+
+static void test_enqueue_peek_consume_enqueue() {
+  queue q;
+  q.resize(1);
+  auto val = re();
+  release_assert(q.try_enqueue(val));
+  auto rp = q.dequeue_peek();
+  release_assert(*rp.first.first == val);
+  q.dequeue_consume();
+  release_assert(q.try_enqueue(val));
+}
+
+static void test_enqueue_one_too_many() {
+  queue q;
+  q.resize(1);
+  auto val = re();
+  release_assert(q.try_enqueue(val));
+  release_assert(q.try_enqueue(val) == false);
+}
+
+static void test_peek_twice() {
+  queue q;
+  q.resize(1);
+  auto val = re();
+  release_assert(q.try_enqueue(val));
+  auto rp = q.dequeue_peek();
+  release_assert(*rp.first.first == val);
+  rp = q.dequeue_peek();
+  release_assert(*rp.first.first == val);
+}
+
+int main(int argc, const char* argv[]) {
+  test_enqueue_noresize();
+  test_peek_noresize();
+  test_consume_noresize();
+  test_enqueue_peek();
+  test_enqueue_peek_enqueue();
+  test_enqueue_peek_consume_peak();
+  test_enqueue_peek_consume_enqueue();
+  test_enqueue_one_too_many();
+  test_peek_twice();
+  return 0;
+}
